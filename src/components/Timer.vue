1ce1d77235ea9b7438b1a237e9f6996b27aed127
kevany23 <kevan23yang@gmail.com>
2023-12-30 02:47:27 -0600

    updated timer

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="timer">
-    <div>
-      {{ timer }}
+    <div class="timer-text">
+      <h2>
+      {{ displayTime() }}
+      </h2>
     </div>
 
     <div class="timer-controls">
@@ -12,21 +14,40 @@
 </template>
 
 <script lang="ts">
+import clock_alarm from '../assets/sounds/clock-alarm-8761.mp3';
 export default {
   data() {
     return {
+      startTime: 0,
+      endTime: 0,
+      duration: 60000,
       timer: 0,
-      intervalId: 0
+      intervalId: 0,
+      alarmSound: new Audio(clock_alarm)
     };
   },
   methods: {
     startTimer() {
+      this.startTime = Date.now();
+      this.endTime = this.startTime + this.duration;
+      console.log(this.startTime);
+      console.log(this.endTime);
       this.intervalId = setInterval(() => {
-        this.timer++;
-      }, 1);
+        this.timer = this.endTime - Date.now();
+        if (this.timer <= 0) {
+          this.timer = 0;
+          this.stopTimer();
+          this.alarmSound.play();
+        }
+      }, 5);
     },
     stopTimer() {
       clearInterval(this.intervalId!);
+    },
+    displayTime() {
+      const minutes = Math.floor(this.timer / 60000);
+      const seconds = Math.floor((this.timer % 60000) / 1000).toString().padStart(2, '0');
+      return `${minutes}:${seconds}`;
     }
   }
 };
@@ -39,6 +60,15 @@ export default {
   flex-direction: column;
   background-color: skyblue;
   padding: 10px;
+  width: 500px;
+  height: 200px;
+  justify-content: center;
+  margin-top: 20px;
+}
+.timer-text  {
+  font-family: monospace;
+  text-align: left;
+  font-size: 40px;
 }
 .timer-controls {
   display: flex;
